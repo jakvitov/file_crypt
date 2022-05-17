fc3b1eef57964d5ed7023a4f20ce3f17c3ea3d31
Jakub Vítovec <jakvitov@gmail.com>
2022-05-17 21:00:20 +0200

    Loading a vector and key from file together implemented

--- src/main/java/cryptFunctions/cryptCentral.java
+++ src/main/java/cryptFunctions/cryptCentral.java
@@ -13,12 +13,14 @@ public class cryptCentral {
 
     //Current loaded key used for encryption
     private SecretKey currentKey;
+    private byte[] loadedInitVector;
     private final String suffix = ".key";
     private final String vectorSuffix = ".vec";
     private final String cyperAlgo = "AES/CBC/PKCS5PADDING";
 
     public cryptCentral(){
         currentKey = null;
+        loadedInitVector = null;
     }
 
     //Given a path to the serialized key, load it from the file
@@ -92,6 +94,23 @@ public class cryptCentral {
         return true;
     }
 
+    //Load innit vector from file with the given targetName
+    public boolean loadInitVector(String targetName){
+        try{
+            ObjectInputStream ooi = new ObjectInputStream(new FileInputStream(targetName + vectorSuffix));
+            this.loadedInitVector = (byte[]) ooi.readObject();
+        }
+        catch (ClassNotFoundException CNFE){
+            System.out.println("Cannot load the vec file.");
+            return false;
+        }
+        catch (IOException IOE){
+            System.out.println("Error while reading from the file.");
+            return false;
+        }
+        return true;
+    }
+
     //Generate new key and initialization vector and save them to the targetName file
     //Check of the keys of this name already exist to prevent from overwriting them
     public boolean generateKeyVecPair(String targetName){
@@ -115,6 +134,34 @@ public class cryptCentral {
         if (saveInitVector(iVector, targetName)){
             return false;
         }
+        return true;
+    }
+
+    //Load the init vector and key of the given name
+    public boolean loadKeyVecPair(String target){
+        File keyFile = new File(target + suffix);
+        File vectorFile = new File(target + vectorSuffix);
+
+        if (keyFile.exists() == false || keyFile.canRead() == false){
+            System.out.println("The key file does not exist or you cannot read it.");
+            return false;
+        }
+
+        if (vectorFile.exists() == false || vectorFile.canRead() == false){
+            System.out.println("The vector file does not exist or you cannot read it.");
+            return false;
+        }
+
+        if (loadKey(target) == false ){
+            System.out.println("Error while loading the key.");
+            return false;
+        }
+
+        if (loadInitVector(target) == false){
+            System.out.println("Loading the init vector failed.");
+            return false;
+        }
+
         return true;
     }
 
