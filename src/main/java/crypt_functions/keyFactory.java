72464f0729ee6eb6392c348d76527ce9d320d0a4
Jakub Vítovec <jakvitov@gmail.com>
2022-05-23 19:44:20 +0200

    Key factory implemented

---
+++ src/main/java/crypt_functions/keyFactory.java
@@ -0,0 +1,58 @@
+package crypt_functions;
+
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * A factory for crating new encryption keys
+ */
+
+public class keyFactory {
+
+    private final String suffix = ".key";
+
+    public keyFactory(){}
+
+    /** Generate new key and return it */
+    public CryptKey generateKey() throws NoSuchAlgorithmException {
+        cryptCentral central = new cryptCentral();
+        return central.generateKeyVecPair();
+    }
+
+    /** Save key into the given file */
+    public void saveKey(CryptKey crKey, String targetFile) throws IOException {
+        String finalName = targetFile + this.suffix;
+        File target = new File(finalName);
+        //In case we would overwrite the given file
+        if (target.exists()){
+            throw new FileAlreadyExistsException(finalName);
+        }
+        try {
+            ObjectOutputStream ooe = new ObjectOutputStream(new FileOutputStream(finalName));
+            ooe.writeObject(crKey);
+        }
+        catch (IOException IOE){
+            throw new IOException(IOE);
+        }
+    }
+    /** Load the key from the given file*/
+    public CryptKey loadKey(String fileName) throws IOException, ClassNotFoundException {
+        String finalName = fileName + this.suffix;
+        File target = new File(finalName);
+        if (target.exists() == false){
+            throw new FileNotFoundException();
+        }
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(finalName));
+            return (CryptKey) ois.readObject();
+        }
+        catch (ClassNotFoundException CNFE){
+            throw new ClassNotFoundException();
+        }
+        catch (IOException IOE){
+            throw new IOException(IOE);
+        }
+    }
+
+}
