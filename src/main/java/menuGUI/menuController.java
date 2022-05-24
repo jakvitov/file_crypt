de4e495716bc095e277eec516721e260cc04ce88
Jakub Vítovec <jakvitov@gmail.com>
2022-05-24 17:18:38 +0200

    First menu implementation concept

--- src/main/java/menuGUI/menuController.java
+++ src/main/java/menuGUI/menuController.java
@@ -2,13 +2,19 @@ package menuGUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 public class menuController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Menu help;
+
+    @FXML
+    private MenuItem action;
+
+    @FXML
+    protected void showHelp(){
+        System.out.println("Show help.");
     }
 }
